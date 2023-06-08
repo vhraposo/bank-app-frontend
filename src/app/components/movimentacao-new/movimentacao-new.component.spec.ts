65737e24db12e10ebffcd6c2c4792e945b226f53
vhraposo <thevhup@gmail.com>
2023-06-08 18:48:50 -0300

    add "movimentacoes" rotes

---
+++ src/app/components/movimentacao-new/movimentacao-new.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovimentacaoNewComponent } from './movimentacao-new.component';
+
+describe('MovimentacaoNewComponent', () => {
+  let component: MovimentacaoNewComponent;
+  let fixture: ComponentFixture<MovimentacaoNewComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MovimentacaoNewComponent]
+    });
+    fixture = TestBed.createComponent(MovimentacaoNewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/movimentacao-new/movimentacao-new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movimentacao-new',
+  templateUrl: './movimentacao-new.component.html',
+  styleUrls: ['./movimentacao-new.component.css']
+})
+export class MovimentacaoNewComponent {
+
+}
