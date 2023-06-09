80e12ff5ad757cadc9899e4055f36a135b0174cb
vhraposo <thevhup@gmail.com>
2023-06-09 09:20:00 -0300

    add "correntista" on new "movimentacao"

--- src/app/components/movimentacao-new/movimentacao-new.component.ts
+++ src/app/components/movimentacao-new/movimentacao-new.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 
 @Component({
   selector: 'app-movimentacao-new',
   templateUrl: './movimentacao-new.component.html',
   styleUrls: ['./movimentacao-new.component.css']
 })
-export class MovimentacaoNewComponent {
+export class MovimentacaoNewComponent implements OnInit {
+  correntistas:any;
+  constructor(
+    private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService,
+  ) { }
+
+  ngOnInit(): void {
+    this.exibirCorrentistas();
+
+  }
+
+  exibirCorrentistas(): void{
+    this.correntistaService.list()
+    .subscribe(
+      data => {
+        this.correntistas = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      })
+  }
 
 }
