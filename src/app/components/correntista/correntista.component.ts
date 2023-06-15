38f7551f21642115b0499d91d2f0cd94ab9d5532
vhraposo <thevhup@gmail.com>
2023-06-15 20:57:45 -0300

    refatoring  doc pdf

--- src/app/components/correntista/correntista.component.ts
+++ src/app/components/correntista/correntista.component.ts
@@ -68,11 +68,16 @@ export class CorrentistaComponent {
     doc.text('Movimentações do Usuário', 10, 20)
 
     doc.setFontSize(16);
-    doc.text(`Nome: ${item.nome}`, 10, 40)
-    doc.text(`Conta N°: ${item.conta.numero}`, 10, 50)
-    doc.text(`Saldo atual: R$ ${item.conta.saldo }`, 10, 60)
+    doc.text('Nome:', 10, 40)
+    doc.text(item.nome, 50, 40)
+    doc.text('Cpf N°:', 10, 50)
+    doc.text(item.cpf, 50, 50)
+    doc.text('Conta N°:', 10, 60);
+    doc.text(item.conta.numero.toString(), 50, 60)
+    doc.text('Saldo atual:', 10, 70)
+    doc.text(`${item.conta.saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`, 50, 70)
 
-    //buscando as movimentações no bd
+    //buscando as movimentações no bd por id da conta
     this.movimentacaoService.findByIdConta(item.id).subscribe(
 
       data => {
@@ -84,27 +89,26 @@ export class CorrentistaComponent {
         const despesas = this.movimentacoes.filter((item: any) => item.tipo === 'DESPESA')
         const receitas = this.movimentacoes.filter((item: any) => item.tipo === 'RECEITA')
 
-        // aqui vai adicionar  as despesas no pdf
-        doc.setFontSize(16);
-        doc.text('Despesas:', 10, 80)
-        let posY = 90;
-        despesas.forEach((despesa: any) => {
-          doc.text(`${despesa.descricao}: ${despesa.valor}`, 10, posY)
-          posY += 10;
-
-        });
-
-        // aqui vai adicionar  as receitas no pdf
-        doc.setFontSize(16);
-        doc.text('Receitas:', 10, posY)
-        posY += 10;
-        receitas.forEach((receita: any) => {
-          doc.text(`${receita.descricao}: ${receita.valor}`, 10, posY)
-          posY += 10;
-        });
+        // aqui vai adicionar  as receitas e desoesas no pdf
+        doc.setFontSize(16)
+        doc.text('Despesas:', 10, 90)
+        doc.text('Receitas:', 100, 90)
+        let posY = 100;
+        const maxLength = Math.max(despesas.length, receitas.length)
+        for (let i = 0; i < maxLength; i++) {
+          if (i < despesas.length) {
+            doc.text(despesas[i].descricao, 10, posY)
+            doc.text(`R$ ${despesas[i].valor}`, 60, posY)
+          }
+          if (i < receitas.length) {
+            doc.text(receitas[i].descricao, 100, posY)
+            doc.text(`R$ ${receitas[i].valor}`, 160, posY)
+          }
+          posY += 10
+        }
 
         // aqui vai salvar os arquivos para download
-          doc.save('relatorio.pdf');
+          doc.save(`Relatorio ${item.nome} - Fx Bank .pdf`)
 
       },
 
