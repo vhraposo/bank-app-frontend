59cf9401466e5e8e9251c8aaccf03ec287334466
vhraposo <thevhup@gmail.com>
2023-06-16 00:34:59 -0300

    add js table on pdf

--- src/app/components/correntista/correntista.component.ts
+++ src/app/components/correntista/correntista.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { CorrentistaService } from 'src/app/services/correntista.service';
 import { jsPDF } from 'jspdf';
-import 'jspdf-autotable';
+import autoTable from 'jspdf-autotable'
+
 import { MovimentacaoService } from 'src/app/services/movimentacao.service'; // Importe o serviço MovimentacaoService aqui
 
 
@@ -65,50 +66,73 @@ export class CorrentistaComponent {
 
     // corpo do pdf
     doc.setFontSize(20);
+
+
+
     doc.text('Movimentações do Usuário', 10, 20)
 
-    doc.setFontSize(16);
-    doc.text('Nome:', 10, 40)
-    doc.text(item.nome, 50, 40)
-    doc.text('Cpf N°:', 10, 50)
-    doc.text(item.cpf, 50, 50)
-    doc.text('Conta N°:', 10, 60);
-    doc.text(item.conta.numero.toString(), 50, 60)
-    doc.text('Saldo atual:', 10, 70)
-    doc.text(`${item.conta.saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`, 50, 70)
+    const nome = item.nome;
+    const cpf = item.cpf;
+    const numeroConta = item.conta.numero;
+    const saldoAtual = item.conta.saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    const tableData = [
+      ['Nome', nome],
+      ['CPF', cpf],
+      ['Conta N°', numeroConta],
+      ['Saldo Atual', saldoAtual]
+    ];
+    const startY = 30;
+    autoTable(doc, {
+      // head: [["Informações Pessoais"] ],
+      body: tableData,
+      startY: startY
+    })
+
 
     //buscando as movimentações no bd por id da conta
     this.movimentacaoService.findByIdConta(item.id).subscribe(
 
       data => {
         this.movimentacoes = data;
-
-
         console.log('hello',this.movimentacoes )
-        //  pegando as receitas e despesas
-        const despesas = this.movimentacoes.filter((item: any) => item.tipo === 'DESPESA')
-        const receitas = this.movimentacoes.filter((item: any) => item.tipo === 'RECEITA')
 
         // aqui vai adicionar  as receitas e desoesas no pdf
         doc.setFontSize(16)
-        doc.text('Despesas:', 10, 90)
-        doc.text('Receitas:', 100, 90)
-        let posY = 100;
-        const maxLength = Math.max(despesas.length, receitas.length)
-        for (let i = 0; i < maxLength; i++) {
-          if (i < despesas.length) {
-            doc.text(despesas[i].descricao, 10, posY)
-            doc.text(`R$ ${despesas[i].valor}`, 60, posY)
+        // autoTable(doc, {
+          //   head: [['Despesas', 'R$', 'Receitas', 'R$']],
+          //   body: [['Pizza', '-35', 'Salario', '2000']]
+          //   })
+
+          //  pegando as receitas e despesas
+          const despesas = this.movimentacoes.filter((item: any) => item.tipo === 'DESPESA')
+          const receitas = this.movimentacoes.filter((item: any) => item.tipo === 'RECEITA')
+          const tableData = [['Despesas', 'R$', 'Receitas', 'R$']];
+
+          const maxLength = Math.max(despesas.length, receitas.length);
+          for (let i = 0; i < maxLength; i++) {
+            const rowData = [
+              i < despesas.length ? despesas[i].descricao : '',
+              i < despesas.length ? `R$ ${despesas[i].valor}` : '',
+              i < receitas.length ? receitas[i].descricao : '',
+              i < receitas.length ? `R$ ${receitas[i].valor}` : ''
+            ];
+
+            tableData.push(rowData);
           }
-          if (i < receitas.length) {
-            doc.text(receitas[i].descricao, 100, posY)
-            doc.text(`R$ ${receitas[i].valor}`, 160, posY)
-          }
-          posY += 10
-        }
+        const pageSize = doc.internal.pageSize;
+        const pageHeight = pageSize.height;
+        const tableHeight = 150
+        const startY = (pageHeight - tableHeight) / 2;
+
+        autoTable(doc, {
+          head: [tableData[0]],
+          body: tableData.slice(1),
+          startY: startY // aqui vai ser a posição inicial da tabela como Y
+        });
 
         // aqui vai salvar os arquivos para download
-          doc.save(`Relatorio ${item.nome} - Fx Bank .pdf`)
+          doc.save(`Relatorio ${item.nome}-Fx Bank.pdf`)
 
       },
 
